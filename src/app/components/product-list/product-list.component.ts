ab824d2f2ef571117eb34de7b9182c40d37086b6
ntwaricharles <charles.ntwari@amalitechtraining.org>
2024-08-20 22:31:58 +0200

    products list

---
+++ src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../../service/cart.service'; // Adjust path as necessary
+import { ProductService, Product } from '../../service/product.service';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+})
+export class ProductListComponent implements OnInit {
+  products: Product[] = [];
+
+  constructor(
+    private productService: ProductService,
+    private cartService: CartService
+  ) {}
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe((data) => {
+      this.products = data;
+    });
+  }
+
+  isAdded(product: Product): boolean {
+    return !!this.cartService
+      .getCartItems()
+      .find((item) => item.name === product.name);
+  }
+
+  getQuantity(product: Product): number {
+    const cartItem = this.cartService
+      .getCartItems()
+      .find((item) => item.name === product.name);
+    return cartItem ? cartItem.quantity : 1;
+  }
+
+  addToCart(product: Product): void {
+    this.cartService.addToCart(product);
+  }
+
+  increment(product: Product): void {
+    const cartItem = this.cartService
+      .getCartItems()
+      .find((item) => item.name === product.name);
+    if (cartItem) {
+      this.cartService.addToCart(cartItem);
+    }
+  }
+
+  decrement(product: Product): void {
+    const cartItem = this.cartService
+      .getCartItems()
+      .find((item) => item.name === product.name);
+    if (cartItem && cartItem.quantity > 1) {
+      cartItem.quantity -= 1;
+    } else {
+      this.cartService.removeFromCart(product);
+    }
+  }
+}
