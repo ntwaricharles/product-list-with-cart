669b520af7aed13a29aad4441e952c5f6a8ec4f8
ntwaricharles <charles.ntwari@amalitechtraining.org>
2024-08-21 11:44:35 +0200

    product list with cart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
-import { ProductItemComponent } from './components/product-list/product-item/product-item.component';
 import { CartComponent } from './components/cart/cart.component';
 import { OrderConfirmationComponent } from './components/order-confirmation/order-confirmation.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent, ProductItemComponent, CartComponent, OrderConfirmationComponent],
+  declarations: [AppComponent, ProductListComponent, CartComponent, OrderConfirmationComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
